de1d3d2e9c99cfae66aa4948d18876af6a3c3179
Aman Raj <rajaman9355@gmail.com>
2025-03-17 16:38:32 +0530

    Added DB Connection

---
+++ Backend/src/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import connectDB from "./db/index.js";
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 4000;
+
+connectDB()
+  .then(() => {
+    app.listen(port, (req, res) => {
+      console.log(`Server running on port: ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Connection to DB Failed", err);
+  });
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
